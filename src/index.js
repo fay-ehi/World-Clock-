0f888e7256e7da3ad9719bcebf37972feb48e8e0
fay-ehi <faithgoddey2002@gmail.com>
2024-11-22 21:19:34 +0100

    Injected current location timezone

--- src/index.js
+++ src/index.js
@@ -33,6 +33,9 @@ setInterval(updateTime, 1000);
 
 function updateCity(event) {
   let cityTimezone = event.target.value;
+  if (cityTimezone == "current") {
+    cityTimezone = moment.tz.guess();
+  }
   let cityName = cityTimezone.replace("-", " ").split("/")[1];
   let cityTime = moment().tz(cityTimezone);
   let cityElement = document.querySelector("#cities");
